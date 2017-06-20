0faf3b95adb51c41d054d10fb276f675d35e3692
huangshouguo <huangshouguo@lvjinsuo.com>
2017-06-20 19:20:14 +0800

    convert java to kotlin

---
+++ app/src/main/java/com/example/hsg/meterialdesignstudy/ui/view/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hsg.meterialdesignstudy.ui.view
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by hsg on 5/15/17.
+ */
+
+open class BaseActivity : AppCompatActivity() {
+
+}
